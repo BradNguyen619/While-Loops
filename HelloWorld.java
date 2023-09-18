53bba84060f3650007b6aa6cf16eb1c16e51af71
Brad Nguyen <brad.nguyen@mrjaffesclass.com>
2023-09-18 11:48:47 -0700

    Code Included in Video

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("i is " + i + " but is less than 10");
+        }
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
+
+
